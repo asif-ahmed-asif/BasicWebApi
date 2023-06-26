87e5c42f0b45914e29933acd2bfc3afef440c1ed
Anonymous <asifahmed1132@gmail.com>
2023-06-26 22:12:45 +0600

    REST API completed for CRUD and search

--- BasicWebApi/Controllers/UserController.cs
+++ BasicWebApi/Controllers/UserController.cs
@@ -40,6 +40,46 @@ namespace BasicWebApi.Controllers
             await _db.SaveChangesAsync();
             return Ok("User successfully added");
         }
+        [HttpPut]
+        public async Task<ActionResult<string>> UpdateUser(User user)
+        {
+            var findUser = await _db.Users.FindAsync(user.Id);
+            if (findUser == null)
+            {
+                return NotFound("User not found");
+            }
+            
+            findUser.Name = user.Name;
+            findUser.Email = user.Email;
+            findUser.Mobile = user.Mobile;
+            findUser.Password = user.Password;
+            await _db.SaveChangesAsync();
+            return Ok("User successfully updated");
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<string>> DeleteUser(int id)
+        {
+            var findUser = await _db.Users.FindAsync(id);
+            if (findUser == null)
+            {
+                return NotFound("User not found");
+            }
+            
+            _db.Users.Remove(findUser);
+            await _db.SaveChangesAsync();
+            return Ok("User successfully deleted");
+        }
+
+        [HttpGet("search/{key}")]
+        public async Task<ActionResult<IEnumerable<User>>> SearchUser(string key)
+        {
+            var user = await _db.Users.Where(s => s.Name.StartsWith(key)).ToListAsync();
+                if (user.Any())
+                {
+                    return Ok(user);
+                }
+            return NotFound("No data Found");
+        }
 
         public IActionResult Index()
         {
--- BasicWebApi/Model/User.cs
+++ BasicWebApi/Model/User.cs
@@ -6,9 +6,15 @@ namespace BasicWebApi.Model
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [Phone]
         public string Mobile { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
