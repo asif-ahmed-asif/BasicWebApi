9a4fa9280b27c6bd21fe27a9d6689114b524b8db
Anonymous <asifahmed1132@gmail.com>
2023-06-27 21:45:41 +0600

    Category Model, Controller and migration created

--- BasicWebApi/Data/ApplicationDataContext.cs
+++ BasicWebApi/Data/ApplicationDataContext.cs
@@ -9,6 +9,7 @@ namespace BasicWebApi.Data
 
 
         public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
 
     }
 }
--- BasicWebApi/Migrations/20230627154120_Category_table_added.Designer.cs
+++ BasicWebApi/Migrations/20230627154120_Category_table_added.Designer.cs
@@ -4,6 +4,7 @@ using BasicWebApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BasicWebApi.Migrations
 {
     [DbContext(typeof(ApplicationDataContext))]
-    partial class ApplicationDataContextModelSnapshot : ModelSnapshot
+    [Migration("20230627154120_Category_table_added")]
+    partial class Category_table_added
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
